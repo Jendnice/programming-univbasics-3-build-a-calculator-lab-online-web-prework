197f07f7d33a2bb5a12f23122741404821fcd662
Jennifer Nicely <pipeline38@hotmail.com>
2019-08-27 22:32:05 +0000

    Automatically backed up by Learn

--- equations.rb
+++ equations.rb
@@ -1,5 +1,26 @@
-def subtraction(num1,num2)
-  num2 - num1
+def addition(num1, num2)
+  num1 + num2
 end
 
-subtraction(3, 5)
+def subtraction(num1, num2)
+  num1 - num2
+end
+
+def division(num1, num2)
+  num1 / num2
+end
+
+def multiplication(num1, num2)
+  num1 * num2
+end
+
+def modulo(num1, num2)
+  num1 % num2
+end
+
+def square_root(num)
+  Math.sqrt(num)
+end
+
+num1 = 5
+num2 = 2 
